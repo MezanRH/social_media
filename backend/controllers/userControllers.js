a8c221ac9f5d2b940705ac73e6fa8fc41bd0aa53
MezanRH <Meaznurr@gmail.com>
2024-06-09 20:55:35 +0600

    add varification

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,5 +1,8 @@
-const { validateEmail, validenam } = require('../helpers/validation')
+const { jwToken } = require('../helpers/token');
+const { validateEmail, validenam, validateUsernaem } = require('../helpers/validation')
 const Users = require('../models/usemodels')
+const bcrypt = require('bcrypt');
+
 
 exports.newUser = async (req, res)=>{
   try {
@@ -41,13 +44,29 @@ exports.newUser = async (req, res)=>{
         message: "Lastname should be minumam 3 and max 15 characters"
       })
     }
+    if(!validenam(password, 8, 40)){
+      return res.status(400).json({
+        message: "password should be minumam 8 and max 40 characters"
+      })
+    }
+
+    // bcrypt-password
+    const crypted = await bcrypt.hash(password, 10)
+
+    // console.log(crypted);
+
+    // validate username
+    let tempUsername = fName + lName
+    let finalUsername = await validateUsernaem(tempUsername)
+
+    // return
 
     const User = await new Users({
       fName,
       lName,
-      username,
+      username: finalUsername,
+      password: crypted,
       email,
-      password,
       bMonth,
       bDay,
       bYear,
@@ -57,9 +76,16 @@ exports.newUser = async (req, res)=>{
     
     res.send(User)
 
+    const emailToken = jwToken({id: User._id.toString()}, "30")
+
+    console.log(emailToken);
+
   } catch (error) {
     res.status(404).json({
       message: "Can not create user"
     })
   }
 }
+
+
+// console.log((+new Date() * Math.random()).toString().substring(0, 1));
