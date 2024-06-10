67ee1542b6e4e39a888c84f445e4c9107a321ebd
MezanRH <Meaznurr@gmail.com>
2024-06-10 07:05:19 +0600

    nodemailer ang google clouds

---
+++ backend/helpers/mailer.js
@@ -0,0 +1,45 @@
+const nodemailer = require("nodemailer");
+const {google} = require('googleapis');
+
+const {OAuth2} = google.auth
+const oauth_link = "https://developers.google.com/oauthplayground/";
+const {EMAIL, MAILING_ID, MAILING_SECRET, MAILING_REFRESH} = process.env;
+
+const auth = new OAuth2(
+  MAILING_ID,
+  MAILING_SECRET,
+  MAILING_REFRESH,
+  oauth_link
+)
+
+exports.sendVerifiedEmail = (email, name, url)=>{
+  auth.setCredentials({
+    refresh_token: MAILING_REFRESH
+  })
+  const accessToken = auth.getAccessToken()
+  const stmp = nodemailer.createTransport(
+    {
+      service: 'gmail',
+      auth:{
+        type: "OAuth2",
+        user: EMAIL,
+        clientId: MAILING_ID,
+        clientSecret: MAILING_SECRET,
+        refreshToken: MAILING_REFRESH,
+        accessToken
+      }
+    }
+  )
+
+  const mailOptions = {
+    from: EMAIL,
+    to: email,
+    subject: "Family Social App",
+    html: ``
+  }
+  stmp.sendMail(mailOptions, (err, res) =>{
+    if(err) return err;
+    return res
+  })
+
+}
