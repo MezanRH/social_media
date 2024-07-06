f0d404361ef3df5ac6fbc3e1030bf3f7b37d25da
MezanRH <Meaznurr@gmail.com>
2024-07-06 17:06:22 +0600

    backend login process complite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import './App.css'
+import Registration from './pages/registration'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+     <Route path='/' element={<Registration/>}/>
+    </Route>
+  )
+);
+
+function App() {
+
+  return (
+    <>
+    <RouterProvider router={router}/>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/registration/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Registration() {
+  return (
+    <div>This is from registration page</div>
+  )
+}
+
+export default Registration
